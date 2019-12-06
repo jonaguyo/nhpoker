cad3cabe62a6b3ef2ea4b7283c1b46d6d5509a87
Jonathan GUYOMARD <jonaguyo@gmail.com>
2019-12-06 16:55:33 +0100

    Départ travail 06/12/2019

--- structure-manager/src/main/java/fr/nhpoker/Main.java
+++ structure-manager/src/main/java/fr/nhpoker/Main.java
@@ -12,15 +12,15 @@ public class Main extends Application
 	{
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws Exception
 	{
 		String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+		String javafxVersion = System.getProperty("javafx.version");
+		Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+		Scene scene = new Scene(new StackPane(l), 640, 480);
+		stage.setScene(scene);
+		stage.show();
 	}
 }
